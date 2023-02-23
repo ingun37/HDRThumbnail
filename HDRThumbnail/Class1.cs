9a88a203bd0dd701c8659eedd13e1b45a7b2ce14
Ingun Jon <ingun37@gmail.com>
2023-02-23 13:55:56 +0900

    wip

--- HDRThumbnail/Class1.cs
+++ HDRThumbnail/Class1.cs
@@ -17,28 +17,26 @@ public class HDRThumbnail
 {
     static byte lin(double x)
     {
-        return (byte)Math.Max(0, Math.Min(255, (x * 255.0)));
+        return (byte)Math.Max(0, Math.Min(255, (Math.Pow(x, 1.0 / 2.2) * 255.0)));
     }
     static void _thumbnail(Stream seekableStream, int width, int height)
     {
         var image = ImageResultFloat.FromStream(seekableStream);
         var meta = ImageInfo.FromStream(seekableStream);
         if (meta == null) throw new InvalidHDRMetadataException();
+        int offsetX = (meta.Value.Width - width) / 2;
+        int offsetY = (meta.Value.Height - height) / 2;
         using (Image<Rgb24> output = new(width, height))
         {
             for (int wi = 0; wi < width; wi++)
             {
                 for (int hi = 0; hi < height; hi++)
                 {
-                    var pi = hi * meta.Value.Width + wi;
+                    var pi = (hi + offsetY) * meta.Value.Width + (wi + offsetX);
 
                     var r = image.Data[pi * 3];
                     var g = image.Data[pi * 3 + 1];
                     var b = image.Data[pi * 3 + 2];
-                    if (1 < r || 1 < g || 1 < b)
-                    {
-                        Console.WriteLine("Big value!");
-                    }
 
                     output[wi, hi] = new Rgb24(lin(r), lin(g), lin(b));
                     // pixels[pi * 3] = (byte)(r * 255);
